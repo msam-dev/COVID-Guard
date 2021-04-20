9be4d8adc822bee8a903d383d770a0ac50be6b67
jetaxe100 <57256192+jetaxe100@users.noreply.github.com>
2021-04-20 18:11:51 +1000

    adding stuff

---
+++ client/covid19-app/src/Screens/Support/Support.js
@@ -0,0 +1,20 @@
+ const Support = () => {
+    return (
+        <div style = {{textAlign: "center", color: "#0E5F76"}}>
+            <h1 style = {{color: "#0E5F76", textAlign: "left", backgroundColor: "#FDC500", paddingLeft: "1%"}}>Support</h1>
+            <h1 style = {{color: "#0E5F76", padding: "2%"}}>COVID Guard Support</h1>
+            For information regarding COVID-19, please click the more info tab at the bottom of this page.
+            <br/>
+            For technical support regarding the COVID Guard app, please use the following services.
+
+            <br/><br/>
+
+            <b>Phone: </b>1800 xxx xxx <br/>
+            <b>Email: </b>COVIDGuard@xxx.com
+
+        </div>
+    )
+};
+
+
+export default Support;
